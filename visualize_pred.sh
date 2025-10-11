f1ba92e6fdee991fd0f574f643d1440adca7c085
jayenliao <jay.chiehen@gmail.com>
2025-10-11 09:41:58 +0800

    feat(visualize): finish cls pred visualization

--- visualize_pred.sh
+++ visualize_pred.sh
@@ -1,5 +1,5 @@
 python visualize_pred.py \
-  --pretrained_ckpt ./logs/pretrain-cls/20251008-150155/vit-t-clf-from_pretrained.pt \
-  --scratch_ckpt ./logs/scratch-cls/20251008-150202/vit-t-clf-from_scratch.pt \
+  --pretrained_ckpt ./outputs/main_exp/pretrain-cls/vit-t-clf-from_pretrained.pt \
+  --scratch_ckpt ./outputs/main_exp/scratch-cls/vit-t-clf-from_scratch.pt \
   --indices 2 9 10 11 12 13 14 15 16 17 18 19\
-  --save_dir logs/cls_viz_selected
+  --save_dir ./outputs/main_exp/cls_viz
